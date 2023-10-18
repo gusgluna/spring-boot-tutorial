a0edb2fcf698915fb4284b72156acfcad20ea4cb
gusgluna <gusgluna@gmail.com>
2023-10-17 21:14:53 -0600

    Implementing find, update and delete api

--- src/main/java/com/gusgluna/springboottutorial/controller/DepartmentController.java
+++ src/main/java/com/gusgluna/springboottutorial/controller/DepartmentController.java
@@ -3,9 +3,9 @@ package com.gusgluna.springboottutorial.controller;
 import com.gusgluna.springboottutorial.entity.Department;
 import com.gusgluna.springboottutorial.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -16,5 +16,25 @@ public class DepartmentController {
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.deleteDepartmentById(departmentId);
+    }
+
+    @PutMapping("departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department){
+        return departmentService.updateDepartment(departmentId, department);
     }
 }
--- src/main/java/com/gusgluna/springboottutorial/service/DepartmentService.java
+++ src/main/java/com/gusgluna/springboottutorial/service/DepartmentService.java
@@ -2,7 +2,17 @@ package com.gusgluna.springboottutorial.service;
 
 import com.gusgluna.springboottutorial.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
 
     Department saveDepartment(Department department);
+
+    List<Department> fetchDepartmentList();
+
+    Department fetchDepartmentById(Long departmentId);
+
+    String deleteDepartmentById(Long departmentId);
+
+    Department updateDepartment(Long departmentId, Department department);
 }
